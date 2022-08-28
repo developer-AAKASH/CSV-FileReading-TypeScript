127950232ed3bd655ebfad68376e876025ac0b6e
developer-AAKASH <201912082@daiict.ac.in>
2022-08-28 16:04:34 +0530

    Refactor with Type Generics to make it more reusable like CSVFileReader class is now usable with any typegit status

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,11 +1,9 @@
 import fs from "fs";
-import {dateStringToDate} from "./utils";
 import {MatchResult} from "./MatchResult";
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-    data: MatchData[] = [];
+// <Type> is for type generics
+export abstract class CsvFileReader<T> {
+    data: T[] = [];
 
     constructor(public fileName: string) {}
 
@@ -16,16 +14,8 @@ export class CsvFileReader {
             .map((row: string) => {
                 return row.split(',');
             })
-            .map((row: string[]): MatchData => {
-                return [
-                    dateStringToDate(row[0]),
-                    row[1],
-                    row[2],
-                    parseInt(row[4]),
-                    parseInt(row[5]),
-                    row[5] as MatchResult, // Type Assertion
-                    row[6]
-                ];
-            });
+            .map(this.mapRow);
     }
+
+    abstract mapRow(row: string[]): T;
 }
